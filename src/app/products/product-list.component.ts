861085462cdf40cba8de743eb1c6c108fab52891
Aleksandar Jovic <ajovic990@gmail.com>
2020-12-29 09:27:30 +0100

    123

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
 })
 
 export class ProductListComponent{
-    pageTitle : string = "Product List"
+    pageTitle : string = "Product List title";
+    aca : boolean=true;
+    imageWidth : number=50;
+    imageMargin :number=2
+    products : any[] =[
+        {
+            "productId":1,
+            "productName":"Sljiva",
+            "productCode":"12345",
+            "avaible":"yes",
+            "price":"1000$",
+            "starRating":5,
+            "imageUrl" : "assets/1.png"
+        },
+        {
+            "productId":2,
+            "productName":"Kruska",
+            "productCode":"12345",
+            "avaible":"yes",
+            "price":"1000$",
+            "starRating":5
+        }
+    ] ;
 }
